7ca24b513df0d7723e247f149083a68a59b2a8fe
김병주 <atker14@gmail.com>
2023-10-10 15:39:04 +0900

    필요없어진 부분 삭제

--- src/main/java/cloud/web/simple/findJdbcDriver/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package cloud.web.simple.findJdbcDriver;
-
-import cloud.web.simple.entity.driver.JdbcDriver;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DriverRepository extends JpaRepository<JdbcDriver, Integer> {
-}
